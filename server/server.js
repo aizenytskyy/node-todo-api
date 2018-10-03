4b873e6226ea8e4f24500694ef7898a288d85d23
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-10-03 18:25:18 +0300

    added mongoose schema for mongodb added create todo REST method

--- server/server.js
+++ server/server.js
@@ -1,32 +1,30 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-//save new smth
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(
+        doc => {
+            res.send(doc);
+        },
+        err => {
+            res.status(400).send(err);
+        }
+    );
 });
 
-var newTodo = new Todo({
-    text: 'create todo app 3',
-    completed: false,
-    completedAt: new Date().getTime()
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-newTodo.save().then(
-    doc => {
-        console.log('Saved todo:', doc);
-    },
-    err => {
-        console.log('Unable to save todo doc');
-    }
-);
