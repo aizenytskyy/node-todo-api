e7b5c1c299820f17a601e73356b1bf833ffc7bd8
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-10-08 17:58:36 +0300

    get todo by id method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
         },
         err => {
             res.status(400).send(err);
+        }
+    );
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then(
+        todo => {
+            if (!todo) {
+                res.status(404).send();
+            }
+            res.send({ todo });
+        },
+        err => {
+            res.status(400).send();
         }
     );
 });
