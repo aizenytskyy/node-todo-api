57c745aeb26fec0ece3efe7e48ea22fb275d5690
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-10-08 18:30:35 +0300

    added tests for get by id method

--- server/server.js
+++ server/server.js
@@ -40,13 +40,13 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send();
+        return res.status(404).send();
     }
 
     Todo.findById(id).then(
         todo => {
             if (!todo) {
-                res.status(404).send();
+                return res.status(404).send();
             }
             res.send({ todo });
         },
