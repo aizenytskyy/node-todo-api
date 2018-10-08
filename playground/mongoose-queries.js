70b4107491fbf2845b55fd076784e9d9203f94cc
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-10-08 15:45:28 +0300

    mongoose queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+var id = '5bbb1736f4be4f30f5e9d65a';
+var userId = '5bb48f58f37da6fcc71ba446';
+
+if (!ObjectID.isValid(id)) {
+    console.log('Id is not valid');
+}
+
+Todo.find({
+    _id: id
+}).then(todos => {
+    console.log('Todos', todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then(todo => {
+    console.log('Todo', todo);
+});
+
+Todo.findById(id)
+    .then(todo => {
+        console.log('Todo', todo);
+    })
+    .catch(e => {
+        console.log(e);
+    });
+
+User.findById(userId)
+    .then(user => {
+        if (!user) {
+            return console.log('Unable to find User');
+        }
+        console.log('User', user);
+    })
+    .catch(e => {
+        console.log(e);
+    });
