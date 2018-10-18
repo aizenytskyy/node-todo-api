6e7d4e28924c0fc1ee7b6a6115cc15196cf22536
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-10-18 11:02:45 +0300

    hash user password before daving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 4444,
-    uid: 'aizen'
-};
+var pswd = 'passw0rd';
 
-var token = jwt.sign(data, 'secret');
-console.log(token);
-var decoded = jwt.verify(token, 'secret');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(pswd, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPswd = '$2a$10$jRLJJdMXSDsKtRYJeV8j1.5h3SECFehD2f5xerjcBR4mmfjBysYNS';
+bcrypt.compare(pswd, hashedPswd, (err, res) => {
+    console.log(res);
+});
+//
+// var data = {
+//     id: 4444,
+//     uid: 'aizen'
+// };
+//
+// var token = jwt.sign(data, 'secret');
+// console.log(token);
+// var decoded = jwt.verify(token, 'secret');
+// console.log(decoded);
 
 // var message = 'user 1';
 // var hash = SHA256(message).toString();
